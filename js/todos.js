3f8db9b799eaafc22b019b8b5b39e28387ac9cd1
tempest200903 <tempest200903@gmail.com>
2015-10-04 14:32:02 +0900

    Parse.initialize

--- js/todos.js
+++ js/todos.js
@@ -7,8 +7,9 @@ $(function() {
   Parse.$ = jQuery;
 
   // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("your-application-id",
-                   "your-javascript-key");
+//  Parse.initialize("your-application-id",
+//                   "your-javascript-key");
+  Parse.initialize("LUf5d6ElfqHV1CytAzJw1nVXpjcJSkN2HjHs4xzi", "taZEYryFsFE66m7Q4CBB3y02DXrqBSkCc3m5UFZJ");
 
   // Todo Model
   // ----------
